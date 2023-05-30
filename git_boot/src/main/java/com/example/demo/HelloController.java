77ede42a2a7b195249967636a343870a976f22d8
soo <susan0915@nate.com>
2023-05-30 17:28:15 +0900

    first commit (변경사항) 업로드

---
+++ git_boot/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/helloboot")
+	public ModelAndView helloboot() {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("model", "스프링 부트를 시작합니다.");
+		mv.setViewName("hello");
+		return mv;
+	}
+	
+	@RequestMapping("/helloajax")
+	@ResponseBody
+	public String helloajax() {
+		return "{\"model\" : \"스프링 부트를 시작합니다.\"}";
+	}
+}
