a49ce014ae59faf0ab9f2b41ff0b3235af703ea3
김수연 <susan0915@nate.com>
2023-05-30 17:50:06 +0900

    sts3 HelloController 수정

--- git_boot/src/main/java/com/example/demo/HelloController.java
+++ git_boot/src/main/java/com/example/demo/HelloController.java
@@ -10,6 +10,7 @@ public class HelloController {
 	
 	@RequestMapping("/helloboot")
 	public ModelAndView helloboot() {
+		System.out.println("sts3에서 추가한 문자열입니다");
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("model", "스프링 부트를 시작합니다(깃허브).");
 		mv.setViewName("hello");
